7e8d66cd293cf703bf9e30cffcab8792d657c051
Ebin Paul <ebinpaul@outlook.com>
2021-05-18 09:44:35 +0530

    Update views.py

--- notes_api/accounts/views.py
+++ notes_api/accounts/views.py
@@ -1,7 +1,30 @@
 from rest_framework import generics
-from .serializers import UserSerializer
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+
+from .serializers import UserSerializer, AuthTokenSerializer
 
 
 class CreateUserView(generics.CreateAPIView):
     # to create new user in the system
     serializer_class = UserSerializer
+
+
+class CreateTokenView(ObtainAuthToken):
+    # Create a new auth token for user
+    serializer_class = AuthTokenSerializer
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+    def post(self, request, *args, **kwargs):
+        serializer = AuthTokenSerializer(data=request.data,
+                                         context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            'token': token.key,
+            'user_id': user.pk,
+            'name': user.name
+        })
