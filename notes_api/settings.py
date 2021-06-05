f597634bbcc089f9896a527ce4a50af85dc19eaa
Ebin Paul <ebinpaul@outlook.com>
2021-06-05 22:27:56 +0530

    changes in docker-compose, requirements, girignore and settings.py

--- notes_api/settings.py
+++ notes_api/settings.py
@@ -96,10 +96,20 @@ WSGI_APPLICATION = 'notes_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'admin',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'db',
+        'PORT': '3306',
     }
 }
 
