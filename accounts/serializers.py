90ba9a781327728faf4ce7fbbba5cface71da9e8
Ebin Paul <ebinpaul@outlook.com>
2022-08-16 18:35:35 +0530

    changes in accounts serializer to resolve linting issues

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -27,9 +27,8 @@ class AuthTokenSerializer(serializers.Serializer):
         email = attrs.get("email")
         password = attrs.get("password")
 
-        user = authenticate(
-            request=self.context.get("request"), username=email, password=password
-        )
+        request = self.context.get("request")
+        user = authenticate(request=request, username=email, password=password)
 
         if not user:
             msg = _("Unable to authenticate with provided credentials")
